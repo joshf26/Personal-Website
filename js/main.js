85e3b85c870e0e6e47d6859e803aa1f235af3020
Joshua Franklin <joshuafranklin26@gmail.com>
2019-01-03 03:11:03 -0500

    Added more ball animations when scrolling furthur and back up.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const ball_inline = $('#ballinline');
 const ball = $('#ball');
-const title = $('#title');
-const about = $('#about');
 
-let document_rect;
 let ball_pos;
 let about_pos;
+let project_pos;
+
+let current_section = 0;
 
 function init() {
-  document_rect = document.body.getBoundingClientRect();
+  let document_rect = document.body.getBoundingClientRect();
 
   let ball_rect = ball_inline.get(0).getBoundingClientRect();
   ball_pos = {
@@ -21,40 +21,84 @@ function init() {
     top: about_rect.top - document_rect.top,
     left: about_rect.left - document_rect.left
   };
+
+  let project_rect = $('#ballpos2').get(0).getBoundingClientRect();
+  project_pos = {
+    top: project_rect.top - document_rect.top,
+    left: project_rect.left - document_rect.left
+  };
 }
 
-function drop() {
-  // Replace the period in the sentence with a ball for animation.
-  ball.css('top', (ball_pos.top + 104) + 'px');
-  ball.css('left', (ball_pos.left + 8) + 'px');
-  ball.css('display', 'block');
-  ball_inline.css('visibility', 'hidden');
-  ball.css('backgroundColor', ball_inline.css('color'));
-
-  // Start the ball as orange and make it grow.
+function go_to_title(current_section) {
   ball.animate({
-    backgroundColor: '#bf360c',
-    left: (about_pos.left + 12) + 'px',
+    top: (ball_pos.top + 128) + 'px',
     height: '15px',
     width: '15px',
+    left: (ball_pos.left + 17) + 'px'
+  }, 1000, function () {
+    ball.css('display', 'none');
+    ball_inline.css('visibility', 'visible');
+  });
+}
+
+function go_to_about(current_section) {
+  if (current_section === 0) {
+    // Replace the period in the sentence with a ball for animation.
+    ball.css('top', (ball_pos.top + 128) + 'px');
+    ball.css('left', (ball_pos.left + 17) + 'px');
+    ball.css('display', 'block');
+    ball_inline.css('visibility', 'hidden');
+    ball.css('backgroundColor', ball_inline.css('color'));
+  }
+
+  // Start the ball as orange and make it shrink.
+  ball.animate({
+    backgroundColor: '#bf360c',
+    left: (about_pos.left + 10) + 'px',
+    height: '10px',
+    width: '10px',
     top: (about_pos.top + 20) + 'px'
   }, 1000);
 
-  $.smoothScroll({
-    scrollTarget: $('#about-target'),
-    speed: 800
-  });
+  setTimeout(function () {
+    ball.css('zIndex', '');
+  }, 500);
+
+  // Scroll the page down to the about section.
+  // $.smoothScroll({
+  //   scrollTarget: $('#about-target'),
+  //   speed: 800
+  // });
 }
 
-function drop2() {
-  $.smoothScroll({
-    scrollTarget: $('#projects-target'),
-    speed: 800
-  });
+function go_to_projects(current_section) {
+  ball.animate({
+    backgroundColor: 'blue',
+    left: '50%',
+    height: '400px',
+    width: '400px',
+    top: (project_pos.top + 30) + 'px'
+  }, 1000);
+
+  setTimeout(function () {
+    ball.css('zIndex', '-1');
+  }, 500);
+
+  // $.smoothScroll({
+  //   scrollTarget: $('#projects-target'),
+  //   speed: 800
+  // });
 }
 
-ball_inline.hover(function () {
-  drop();
+const section_animations = [go_to_title, go_to_about, go_to_projects, function () {}];
+
+document.addEventListener('scroll', function () {
+  let documentTop = $(window).scrollTop();
+  let new_section = Math.round(documentTop / window.innerHeight);
+  if (new_section !== current_section) {
+    section_animations[new_section](current_section);
+    current_section = new_section;
+  }
 });
 
 init();
