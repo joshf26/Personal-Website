0ab091cc5f906493af0cf65d646aabad2b73dadc
Joshua Franklin <joshuafranklin26@gmail.com>
2020-12-30 15:13:04 -0500

    Update resume

--- src/static/resume/index.js
+++ src/static/resume/index.js
@@ -41,7 +41,7 @@ const possibleInputs = {
     'action': function() {
       print('<b>Computer Science BS and Space Minor, May 2021</b>');
       print('<i>University of Colorado at Boulder</i>');
-      print('Overall Cumulative GPA: <b>3.76</b>');
+      print('Overall Cumulative GPA: <b>3.83</b>');
       print('Computer Science GPA: <b>4.0</b>');
     }
   },
